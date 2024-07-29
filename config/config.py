59823308b3867166c771de140106a301866c35df
Redo7 <szczeposlaw668@gmail.com>
2024-07-29 19:32:26 +0200

    Add a database entry on server join

--- config/config.py
+++ config/config.py
@@ -6,31 +6,35 @@ database = sqlite3.connect('config/config.db')
 cursor = database.cursor()
 database.execute('CREATE TABLE IF NOT EXISTS config(server_id INT, twitter_links INT DEFAULT 1, animation_3c INT DEFAULT 1)')
 
+@bot.event
+async def on_guild_join(guild):
+    query = 'SELECT * FROM config WHERE server_id = ?'
+    data = cursor.execute(query, (guild.id,)).fetchall()   
+    if data != [] and guild.id == data[0][0]:
+        print(f'Joined {guild} ({guild.id}), but found an existing database entry. Skipping...')
+        return
+    else:
+        print(f'Creating new database entry for {guild} ({guild.id})')
+        query = 'INSERT INTO config (server_id) VALUES (?)'
+        cursor.execute(query, (guild.id,))
+        database.commit()
+
 def update_db(server_id, feature):
     # Check db for entries
     query = 'SELECT * FROM config WHERE server_id = ?'
     data = cursor.execute(query, (server_id,)).fetchall()
-    if data != [] and server_id == data[0][0]:
-        # Get value
-        query = 'SELECT "{}" FROM config WHERE server_id = ?'.format(feature)
-        current_state = cursor.execute(query, (server_id,)).fetchall()
-        # Flip value
-        current_state = current_state[0][0]
-        current_state ^= 1
-        # Set new value
-        query = 'UPDATE config SET "{}" = ? WHERE server_id = ?'.format(feature)
-        cursor.execute(query, (current_state, server_id))
-        database.commit()
-        print(f'Database changes: ({feature}: {current_state}) for server: {server_id} committed successfully')
-        return current_state
-    else:
-        # Create new entry if not found
-        print(f"couldn't find {server_id}... Adding a new entry")
-        query = 'INSERT INTO config (server_id) VALUES (?)'
-        cursor.execute(query, (server_id,))
-        database.commit()
-        print('Database changes committed successfully')
-        update_db(server_id, feature)
+    # Get value
+    query = 'SELECT "{}" FROM config WHERE server_id = ?'.format(feature)
+    current_state = cursor.execute(query, (server_id,)).fetchall()
+    # Flip value
+    current_state = current_state[0][0]
+    current_state ^= 1
+    # Set new value
+    query = 'UPDATE config SET "{}" = ? WHERE server_id = ?'.format(feature)
+    cursor.execute(query, (current_state, server_id))
+    database.commit()
+    print(f'Database changes: ({feature}: {current_state}) for server: {server_id} committed successfully')
+    return current_state
 
 def check_db(server_id, feature):
     query = 'SELECT "{}" FROM config WHERE server_id = "{}"'.format(feature, server_id)
