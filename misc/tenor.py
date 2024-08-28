1068d73e6543d8e01be5cbcca09e1223bf329aae
Redo7 <szczeposlaw668@gmail.com>
2024-08-28 20:48:28 +0200

    Fix `anime` value check. Add more gif options

--- misc/tenor.py
+++ misc/tenor.py
@@ -9,9 +9,12 @@ ckey = CLIENT_KEY
 
 @bot.hybrid_command(name="gifs", description="Send a gif targeted at someone")
 @app_commands.choices(name=[
+    Choice(name="Bonk", value="bonk"),
+    Choice(name="Chase", value="chase"),
     Choice(name="Cheer", value="cheer"),
     Choice(name="Clap", value="clap"),
     Choice(name="Confused", value="confused"),
+    Choice(name="Cover Mouth", value="cover someone elses mouth"),
     Choice(name="High Five", value="high five"),
     Choice(name="Homie Kiss", value="homie kiss"),
     Choice(name="Hug", value="hug"),
@@ -19,8 +22,11 @@ ckey = CLIENT_KEY
     Choice(name="Kiss", value="kiss"),
     Choice(name="Kys?", value="kys"),
     Choice(name="Peek", value="peek"),
+    Choice(name="Throw", value="throw someone"),
     Choice(name="Sad", value="sad"),
-    Choice(name="Slap", value="slap"),
+    Choice(name="Slap", value="slapping"),
+    Choice(name="Slap (Competition)", value="slapping competition"),
+    Choice(name="Spit", value="spit"),
     Choice(name="Wave", value="wave")
     ])
 @app_commands.choices(anime=[
@@ -35,19 +41,25 @@ async def gifs(
     ) -> None:
 
     resp = {
-        'cheer': 'cheers for',
-        'clap': 'claps for',
-        'confused': 'is confused about',
-        'high five': 'high fives',
-        'homie kiss': 'homie kisses',
-        'hug': 'hugs',
-        'kick': 'kicks',
-        'kiss': 'kisses',
+        'bonk': f'bonks {target}',
+        'chase': f'runs after {target}',
+        'cheer': f'cheers for {target}',
+        'clap': f'claps for {target}',
+        'confused': f'is confused about {target}',
+        'cover someone elses mouth': f"covers {target}'s mouth",
+        'high five': f'high fives {target}',
+        'homie kiss': f'homie kisses {target}',
+        'hug': f'hugs {target}',
+        'kick': f'kicks {target}',
+        'kiss': f'kisses {target}',
         'kys': '',
-        'peek': 'peeks at',
-        'sad': 'is sad for',
-        'slap': 'slaps',
-        'wave': 'waves at'
+        'peek': f'peeks at {target}',
+        'throw someone': f'yeets {target}',
+        'sad': f'is sad for {target}',
+        'slapping': f'slaps {target}',
+        'slapping competition': f'slaps {target}',
+        'spit': f'spits on {target}',
+        'wave': f'waves at {target}'
     }
 
     kysRandInt = random.randint(0, 1)
@@ -63,7 +75,7 @@ async def gifs(
         pass
 
     lmt = 8 # amount of gifs to retrieve
-    if anime.value == 1:
+    if anime is not None and anime.value == 1:
         r = requests.get(
             "https://tenor.googleapis.com/v2/search?q=%s+anime&key=%s&client_key=%s&limit=%s" % (name.value, apikey, ckey,  lmt))
     else:
@@ -72,7 +84,7 @@ async def gifs(
         
     if r.status_code == 200:
         gifs = json.loads(r.content)
-        await ctx.send(f"{ctx.author.display_name} {resp[name.value]} {target}")
+        await ctx.send(f"{ctx.author.display_name} {resp[name.value]}")
         await ctx.send(gifs['results'][random.randint(0, 7)]['url'])
     else:
         gifs = None
