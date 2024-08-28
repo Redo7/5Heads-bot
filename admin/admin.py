90b0bb771a51161d2989dbbeef650972600d14fe
Redo7 <szczeposlaw668@gmail.com>
2024-08-28 20:00:40 +0200

    Fix purge embeds

--- admin/admin.py
+++ admin/admin.py
@@ -3,6 +3,7 @@ from imports import *
 @commands.has_permissions(manage_messages=True)
 @bot.hybrid_command(name='purge', brief='Deletes a specified number of messages from the current channel')
 async def purge(ctx, amount: int):
+  await ctx.send(f'Purging messages...')
   deleted = await ctx.channel.purge(limit=amount)
   if len(deleted) == 0:
     embed = discord.Embed(title='Purge "complete"', color=0xED1B53)
