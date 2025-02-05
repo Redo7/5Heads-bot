493c4c088fb7f36fd3130795364f223cc8303b1c
Redo7 <szczeposlaw668@gmail.com>
2025-02-05 16:59:44 +0100

    Fix column label

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -378,9 +378,9 @@ class RouletteView(ui.View):
             super().__init__(
                 placeholder='Pick a range',
                 options=[
-                    discord.SelectOption(label='1-12', value=1),
-                    discord.SelectOption(label='13-24', value=2),
-                    discord.SelectOption(label='25-36', value=3)
+                    discord.SelectOption(label='1,4,7,10,13,16,19,22,25,28,31,34', value=1),
+                    discord.SelectOption(label='2,5,6,8,11,14,17,20,23,26,29,32,35', value=2),
+                    discord.SelectOption(label='3,6,9,12,15,18,21,24,27,30,33,36', value=3)
                 ])
             self.bet = bet
             self.economy = economy
